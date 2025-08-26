3b598b0720139bb69c4af22a29d4d44e07b47dd5
Emílio Marcelino Honório Branquinho <emiliomarcelino400@gmail.com>
2025-08-26 15:26:27 -0700

    upando atualizacoes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,28 @@
 import { Link } from "react-router";
-
+import { LogOut } from "lucide-react";
+import { signOut } from "firebase/auth";
+import {auth} from '../../services/firebaseConnection';
 
 export function Header(){
+
+    async function handleLogout(){
+        await signOut(auth)        
+    }
+
+
     return(
         <>
-        <header className="bg-white text-black">
-            <div className="flex items-center h-[50px]">
-                <Link to="/" >Home</Link>
-                
-            </div>
+        <header className="w-full lg:px-[350px] lg:py-[10px] text-black ">
+            <nav className="bg-white flex items-center justify-between h-10 rounded-sm">
+                <div className="flex gap-5 px-5">
+                    <Link to="/">Home</Link>
+                    <Link to="/admin">Admin</Link>
+                    <Link to="/admin/social">Redes Sociais</Link>
+                </div>
+                <button onClick={handleLogout} className="px-3">
+                    <LogOut color="red" cursor="pointer"/>
+                </button>
+            </nav>
         </header>
         </>
     )
--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,17 +1,77 @@
-import { Header } from "../../components/Header"
+import { useState } from "react";
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+import { Link } from 'lucide-react';
 
 export function Admin(){
+
+    const[inputName, setInputName] = useState("");
+    const[inputUrl, setInputUrl] = useState("");
+    const[NameColorInput, setNameColorInput] = useState("#f1f1f1");
+    const[bgColorInput, setbgColorInput] = useState("#0000");
+
     return(
         <>
        <Header/>
-        <div className="h-screen flex justify-center items-center bg-[#030129]">
-        <div className="flex flex-col">
-                <label className="mt-2" htmlFor="name">Nome do link</label>
-                <input className="bg-[#172554] w-[500px] h-[35px] rounded-sm indent-3 mt-2" type="text" />
-                <label className="mt-2" htmlFor="url">URL do link</label>
-                <input className="bg-[#172554] w-[500px] h-[35px] rounded-sm indent-3 mt-2" type="text" />
-                
-        </div>
+        <div className="h-screen flex justify-center bg-[#030129]">
+        <form action="">
+            <div className="flex flex-col">
+                    <label className="pt-6" htmlFor="name">Nome do link</label>
+                    <Input
+                    className="bg-white w-[500px] h-[30px] rounded-xs indent-3 text-black mt-4"
+                    placeholder="Digite o nome do link:"
+                    value={inputName}
+                    onChange={(e) =>{setInputName(e.target.value)}}
+                    />
+                    <label className="pt-6" htmlFor="url">URL do link</label>
+                    <Input
+                    className="bg-white w-[500px] h-[30px] rounded-xs indent-3 text-black mt-4"
+                    placeholder="Digite a URL do link:"
+                    value={inputUrl}
+                    onChange={(e) =>{setInputUrl(e.target.value)}}
+                    />
+            </div>
+
+            <section>
+                <div className="flex gap-10 items-center h-20">
+                    <div className="flex gap-3">
+                        <label htmlFor="">Cor do link</label>
+                        <input 
+                        className="h-9 w-10 mb-2 " 
+                        type="color"
+                        value={NameColorInput}
+                        onChange={(e) => setNameColorInput(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="flex gap-3">
+                        <label htmlFor="">Fundo do link</label>
+                        <input 
+                        className="h-9 w-10 mb-2 " 
+                        type="color"
+                        value={bgColorInput}
+                        onChange={(e)=>{setbgColorInput(e.target.value)}}
+                        />
+                    </div>
+                </div>
+            </section>
+
+            <div className="border  border-blue-600 flex flex-col items-center justify-start h-[100px] gap-5">
+                <label htmlFor="">Veja como está ficando:</label>
+                <article
+                className="h-[35px] rounded-sm w-[420px] flex items-center justify-center"
+                style={{background: bgColorInput}}
+                >
+                    <p
+                    style={{color: NameColorInput}}
+                    >
+                    {inputName}
+                    </p>
+                </article>
+            </div>
+
+            <button className="flex items-center justify-center gap-3 bg-blue-600 w-full mt-2 rounded-sm h-[35px] cursor-pointer">cadastrar <i><Link size={18}/></i></button>
+        </form>
         </div>
         </>
     )
